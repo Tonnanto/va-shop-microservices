a132ba56d58b214063b84525c7fa8ebf39010fa9
Anton Stamme <anton@stamme.de>
2022-03-11 23:04:27 +0100

    Shop SpringApplication (WIP)

---
+++ shop/src/main/java/de/leuphana/va/onlineshop/shop/component/behaviour/ShopApplication.java
@@ -0,0 +1,11 @@
+package de.leuphana.va.onlineshop.shop.component.behaviour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
+}
