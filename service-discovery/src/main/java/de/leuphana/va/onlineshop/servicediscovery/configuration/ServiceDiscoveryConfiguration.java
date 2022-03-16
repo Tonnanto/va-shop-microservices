1c86f90e7f184e9b7d867ec794bdd751a9858775
Anton Stamme <anton@stamme.de>
2022-03-16 20:27:14 +0100

    api-gateway microservice

--- service-discovery/src/main/java/de/leuphana/va/onlineshop/servicediscovery/configuration/ServiceDiscoveryConfiguration.java
+++ service-discovery/src/main/java/de/leuphana/va/onlineshop/servicediscovery/configuration/ServiceDiscoveryConfiguration.java
@@ -1,9 +1,7 @@
 package de.leuphana.va.onlineshop.servicediscovery.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 public class ServiceDiscoveryConfiguration {
 }
